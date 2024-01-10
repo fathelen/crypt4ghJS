b8a7dd86819d06f18c6631076eff1026a4e10709
fathelen <fabienne.thelen@outlook.de>
2024-01-10 15:55:43 +0100

    test back

--- src/index.js
+++ src/index.js
@@ -1,4 +1,3 @@
-/*
 const keygen = require('./keygen')
 const keyfiles = require('./check_keyfiles')
 const encryption = require('./encryption')
@@ -6,30 +5,6 @@ const decryption = require('./decryption')
 const reeencryption = require('./reeencryption')
 const rearrangment = require('./rearrange')
 const checkFileformat = require('./check_fileformat')
-*/
-import { encryption_keyfiles } from './check_keyfiles'
-import { decryption } from './decryption'
-import { keygen } from './keygen'
-import { encryption } from './encryption'
-import { reencrypt } from './reeencryption'
-import { rearrange } from './rearrange'
-
-// Decryption
-document.getElementById('input').addEventListener('change', function (e) {
-  const file = document.getElementById('input').files[0]
-  const file2 = document.getElementById('input').files[1]
-  const password = document.getElementById('psw').value
-  const blocks = document.getElementById('block').value;
-  (
-    async () => {
-      const seckeyFile = await file.text()
-      const block = blocks.split(',')
-      const fileContents = document.getElementById('filecontents')
-      const keys = await encryption_keyfiles([seckeyFile], password)
-      const plaintext = await decryption(file2, keys[0], block)
-      fileContents.innerText = plaintext
-    })()
-})
 
 // KeyGen
 const button = document.getElementById('submit')
@@ -37,7 +12,7 @@ button.addEventListener('click', async function (event) {
   const seckey = document.getElementById('seckeyfile')
   seckey.innerText = 'hallo'
   const password = await document.getElementById('psw2').value
-  const result = keygen(password)
+  const result = keygen.keygen(password)
   const erg = await result
   const pubkey = document.getElementById('pubkeyfile')
   pubkey.innerText = erg[1]
@@ -45,4 +20,4 @@ button.addEventListener('click', async function (event) {
   // seckey.innerText = erg[0]
 })
 
-// module.exports = { keygen, keyfiles, encryption, decryption, reeencryption, rearrangment, check_fileformat: checkFileformat }
+module.exports = { keygen, keyfiles, encryption, decryption, reeencryption, rearrangment, check_fileformat: checkFileformat }
