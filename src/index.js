ab98e754b9e90bf792d3bf45664ed5c9b0f44618
fathelen <fabienne.thelen@outlook.de>
2024-01-10 15:22:44 +0100

    index.js

--- src/index.js
+++ src/index.js
@@ -28,11 +28,13 @@ document.getElementById('input').addEventListener('change', function (e) {
 // KeyGen
 const button = document.getElementById('submit')
 button.addEventListener('click', async function (event) {
+  const seckey = document.getElementById('seckeyfile')
+  seckey.innerText = 'hallo'
   const password = await document.getElementById('psw2').value
   const result = keygen.keygen(password)
   const erg = await result
   const pubkey = document.getElementById('pubkeyfile')
   pubkey.innerText = erg[1]
-  const seckey = document.getElementById('seckeyfile')
-  seckey.innerText = erg[0]
+  // const seckey = document.getElementById('seckeyfile')
+  // seckey.innerText = erg[0]
 })
