4655c6bd2cd2a6499257da13dc1a4dad803ccf4e
fathelen <fabienne.thelen@outlook.de>
2024-01-10 14:55:20 +0100

    decryption for html

--- src/index.js
+++ src/index.js
@@ -7,3 +7,20 @@ const rearrangment = require('./rearrange')
 const checkFileformat = require('./check_fileformat')
 
 module.exports = { keygen, keyfiles, encryption, decryption, reeencryption, rearrangment, check_fileformat: checkFileformat }
+
+// Decryption
+document.getElementById('input').addEventListener('change', function (e) {
+  const file = document.getElementById('input').files[0]
+  const file2 = document.getElementById('input').files[1]
+  const password = document.getElementById('psw').value
+  const blocks = document.getElementById('block').value;
+  (
+    async () => {
+      const seckeyFile = await file.text()
+      const block = blocks.split(',')
+      const fileContents = document.getElementById('filecontents')
+      const keys = await keyfiles.encryption_keyfiles([seckeyFile], password)
+      const plaintext = await decryption(file2, keys[0], block)
+      fileContents.innerText = plaintext
+    })()
+})
