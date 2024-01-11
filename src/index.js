0230c8b665505ff43e786ab2eabedc7a00086e6f
fathelen <fabienne.thelen@outlook.de>
2024-01-11 15:01:50 +0100

    decryption index.js

--- src/index.js
+++ src/index.js
@@ -6,6 +6,27 @@ const reeencryption = require('./reeencryption')
 const rearrangment = require('./rearrange')
 const checkFileformat = require('./check_fileformat')
 
+// Decryption
+
+document.getElementById('input').addEventListener('change', function (e) {
+  const file = document.getElementById('input').files[0]
+  const file2 = document.getElementById('input').files[1]
+  const password = document.getElementById('psw').value
+  const blocks = document.getElementById('block').value;
+  (async () => {
+    const seckeyFile = await file.text()
+    const keys = await keyfiles.encryption_keyfiles([seckeyFile], password)
+    const fileContents = document.getElementById('filecontents')
+    fileContents.innerText = keys
+    /*
+    const block = blocks.split(',')
+    const fileContents = document.getElementById('filecontents')
+    const keys = await keyfiles.encryption_keyfiles([seckeyFile], password)
+    const plaintext = await decryption.pureDecryption(file2, keys[0], block)
+    fileContents.innerText = plaintext */
+  })()
+})
+
 // KeyGen
 const button = document.getElementById('submit')
 button.addEventListener('click', async function (event) {
