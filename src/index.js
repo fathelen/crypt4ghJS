a450f1d839e525cca71a348048d90d795a754ec8
fathelen <fabienne.thelen@outlook.de>
2024-01-14 20:43:03 +0100

    decryption index.js

--- src/index.js
+++ src/index.js
@@ -10,10 +10,15 @@ document.getElementById('input').addEventListener('change', function (e) {
   const file = document.getElementById('input').files[0]
   const file2 = document.getElementById('input').files[1]
   const password = document.getElementById('psw').value
-  const blocks = document.getElementById('block').value;
+  const blocks = document.getElementById('block').value
+  let block = [] | null;
   (async () => {
     const seckeyFile = await file.text()
-    const block = blocks.split(',')
+    if (blocks === '') {
+      block = null
+    } else {
+      block = blocks.split(',')
+    }
     const fileContents = document.getElementById('filecontents')
     const keys = await keyfiles.encryption_keyfiles([seckeyFile], password)
     const headerChunk = await file2.slice(0, 1000)
