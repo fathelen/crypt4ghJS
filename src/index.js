13a375130f254872860bf9b58ec95942d8a862e7
fathelen <fabienne.thelen@outlook.de>
2024-01-18 11:11:02 +0100

    new html encryption

--- src/index.js
+++ src/index.js
@@ -33,6 +33,8 @@ button.addEventListener('click', async function (event) {
 })
 
 // Encryption
+document.getElementById('button').onclick = function () { myFunction() }
+
 function myFunction () {
   document.getElementById('button').innerHTML = 'YOU CLICKED ME!'
 }
