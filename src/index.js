7805385938ac776933a72bf873fceff3831ab2a6
fathelen <fabienne.thelen@outlook.de>
2024-01-18 11:35:51 +0100

    new html encryption

--- src/index.js
+++ src/index.js
@@ -36,7 +36,8 @@ button.addEventListener('click', async function (event) {
 document.getElementById('button').onclick = function () { myFunction() }
 
 function myFunction () {
-  const file = document.getElementById('input')
+  const file = document.getElementById('input').textContent
+  console.log(file)
   const file2 = document.getElementById('input2')
   const file3 = document.getElementById('input3')
   const password = document.getElementById('psw2').value
@@ -46,7 +47,6 @@ function myFunction () {
 
   (async () => {
     const seckeyFile = await file.text()
-    console.log(seckeyFile)
     const pubkeyFile = await file2.text()
     const block = null
     const keys = await keyfiles.encryption_keyfiles([seckeyFile, pubkeyFile], password)
